9113bf6c361b9988f4c4cf98ffd1ccc4da830c91
zayn <mdrahul6967@gmail.com>
2022-07-25 05:41:52 +0600

    hgbfvc

--- src/components/player/Moon.jsx
+++ src/components/player/Moon.jsx
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom';
 
 
 function Player() {
-    var h = window.innerHeight;
-    var hh=h*.6
+    var h = window.innerWidth;
+    var hh=h*.9
     var srcc="https://firebasestorage.googleapis.com/v0/b/jsprj-b0693.appspot.com/o/1.webm?alt=media&token=ac4c3f6e-2a07-42c3-a3df-df139c6ac4a0"
     return (<>
 
-        <video id="video" controls height={hh} autoPlay >
+        <video id="video" controls width={hh} autoPlay >
             <source id="source" src={srcc} srcSet=""></source>
         </video>
 
